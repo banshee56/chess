91fbde51600b44f12f3078febf8d213384b27411
Bansharee Ireen <bansharee@Bansharees-MacBook-Pro.local>
2022-10-23 15:11:58 -0400

    not using

--- MinimaxAI.py
+++ MinimaxAI.py
@@ -8,94 +8,103 @@ class State():
         self.board = board
         self.depth = 0
 
-        # # to quickly check what piece is at a location we want to move to
-        # letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-        # nums = ['1', '2', '3', '4', '5', '6', '7', '8']
-        # locations = {}
-        # # initialize
-        # for letter in letters:
-        #     for num in nums:
-        #         locations[str(letter+num)] = None
+        # to quickly check what piece is at a location we want to move to
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        nums = ['1', '2', '3', '4', '5', '6', '7', '8']
+        locations = {}
+        # initialize
+        for letter in letters:
+            for num in nums:
+                locations[str(letter+num)] = None
 
-        # # a collection of pieces
-        # black = []
-        # white = []
+        # a collection of pieces
+        black = []
+        white = []
 
-        # # mapping locations to the occupying pieces
-        # row = 8
-        # col = 1 # = ord('a')-96 
-        # for char in str(board):
-        #     if col > 8:
-        #         col = 1
+        # mapping locations to the occupying pieces
+        row = 8
+        col = 1 # = ord('a')-96 
+        for char in str(board):
+            if col > 8:
+                col = 1
 
-        #     if char == ' ':
-        #         continue
+            if char == ' ':
+                continue
 
-        #     if char == '.':
-        #         col += 1
-        #         continue
-        #     if char == '\n':
-        #         row -= 1
-        #         continue
+            if char == '.':
+                col += 1
+                continue
+            if char == '\n':
+                row -= 1
+                continue
 
-        #     # white pieces are capital letters with ord() values < 97 ('a')
-        #     if ord(char) < 97:
-        #         white.append(char)
+            # white pieces are capital letters with ord() values < 97 ('a')
+            if ord(char) < 97:
+                if char == 'P':
+                    type = 0
+                    val = 1
+                if char == 'N':
+                    type = 1
+                    val = 3
+                if char == 'B':
+                    type = 2
+                    val = 3
+                if char == 'R':
+                    type = 3
+                    val = 5
+                if char == 'Q':
+                    type = 4
+                    val = 9
+                if char == 'K':
+                    type = 5
+                    val = None
 
-        #         if char == 'P':
-        #             type = 0
-        #             val = 1
-        #         if char == 'N':
-        #             type = 1
-        #             val = 3
-        #         if char == 'B':
-        #             type = 2
-        #             val = 3
-        #         if char == 'R':
-        #             type = 3
-        #             val = 5
-        #         if char == 'Q':
-        #             type = 4
-        #             val = 9
-        #         if char == 'K':
-        #             type = 5
-        #             val = None
+                piece = Piece(type, val, 0, str(chr(col+96))+str(row))
+                locations[str(chr(col+96))+str(row)] = piece
+                white.append(piece)
 
-        #         locations[str(chr(col+96))+str(row)] = Piece(type, val, 0, str(chr(col+96))+str(row))
+            else:
+                if char == 'p':
+                    type = 0
+                    val = 1
+                if char == 'n':
+                    type = 1
+                    val = 3
+                if char == 'b':
+                    type = 2
+                    val = 3
+                if char == 'r':
+                    type = 3
+                    val = 5
+                if char == 'q':
+                    type = 4
+                    val = 9
+                if char == 'k':
+                    type = 5
+                    val = None
 
-        #     else:
-        #         black.append(char)
+                piece = Piece(type, val, 1, str(chr(col+96))+str(row))
+                locations[str(chr(col+96))+str(row)] = piece
+                black.append(piece)
 
-        #         if char == 'p':
-        #             type = 0
-        #             val = 1
-        #         if char == 'n':
-        #             type = 1
-        #             val = 3
-        #         if char == 'b':
-        #             type = 2
-        #             val = 3
-        #         if char == 'r':
-        #             type = 3
-        #             val = 5
-        #         if char == 'q':
-        #             type = 4
-        #             val = 9
-        #         if char == 'k':
-        #             type = 5
-        #             val = None
+            col += 1
 
-        #         locations[str(chr(col+96))+str(row)] = Piece(type, val, 1, str(chr(col+96))+str(row))
-        #     col += 1
-
-        # self.locations = locations
-        # self.black_pieces = black
-        # self.white_pieces = white
+        self.locations = locations
+        self.black_pieces = black
+        self.white_pieces = white
+            
 
 class MinimaxAI():
     def __init__(self, color, depth):
         self.color = color
         self.depth_limit =  depth
+
+        if self.color == chess.WHITE:
+            enemy = chess.BLACK
+        else:
+            enemy = chess.WHITE
+
+        self.enemy = enemy
 
     def choose_move(self, board):
         state = State(board)
@@ -106,62 +115,26 @@ class MinimaxAI():
     def evaluation_fn(self, state):
         # if terminal, win > draw > loss -> 0
         if self.terminal_test(state):
-            if self.color == chess.WHITE:
-                enemy = chess.BLACK
-            else:
-                enemy = chess.WHITE
-
             outcome = state.board.outcome()
 
             # AI won
             if outcome.winner == self.color:
                 return 78
             # loss
-            elif outcome.winner == enemy:
+            elif outcome.winner == self.enemy:
                 return -78
             # draw
             else:
                 return 0
 
         # if non-terminal, weighted points
-        (white, black) = self.find_pieces(state)
 
         white_score = 0
-        for char in white:
-            if char == 'K':
-                continue  
-            elif char == 'P':
-                val = 1
-            elif char == 'N':
-                val = 3
-            elif char == 'B':
-                val = 3
-            elif char == 'R':
-                val = 5
-            elif char == 'Q':
-                val = 9
-            
-            white_score += val
-
-        black_score = 0
-        for char in black:
-            if char == 'k':
-                continue
-            elif char == 'p':
-                val = 1
-            elif char == 'n':
-                val = 3
-            elif char == 'b':
-                val = 3
-            elif char == 'r':
-                val = 5
-            elif char == 'q':
-                val = 9
-    
-            black_score += val
+ 
+        black_score += val
         
         # minimax is white
-        if self.color == 0:
+        if self.color == chess.WHITE:
             score = white_score - black_score
 
         # it is black
@@ -193,6 +166,7 @@ class MinimaxAI():
         for move in moves:
             self.make_move(state, move)
 
+            # get the move that leads to max utility
             utility = self.min_value(state)
             if utility >  arg_max:
                 arg_max = utility
@@ -205,36 +179,29 @@ class MinimaxAI():
 
     def max_value(self, state):
         if self.cutoff_test(state):
-            # before  = time()
             utility = self.evaluation_fn(state)
-            # after = time()
-            # print(after-before)
             return utility
         
         v = -float('inf')
 
         for move in list(state.board.legal_moves):
-            self.make_move(state, move)
+            (origin_piece, end_peice) = self.make_move(state, move)
             v = max(v, self.min_value(state))
-            self.undo_move(state)
+            self.undo_move(state, move, origin_piece, end_peice)
 
         return v
 
     def min_value(self, state):
         if self.cutoff_test(state):
-            # before  = time()
             utility = self.evaluation_fn(state)
-            # after = time()
-            # print(after-before)
             return utility
         
         v = float('inf')
 
         for move in list(state.board.legal_moves):
-            # prev_piece = state.locations[move[:2]]
-            self.make_move(state, move)
+            (origin_piece, end_peice) = self.make_move(state, move)
             v = min(v, self.max_value(state))
-            self.undo_move(state)
+            self.undo_move(state, move, origin_piece, end_peice)
 
         return v
 
@@ -242,18 +209,35 @@ class MinimaxAI():
         state.depth += 1
         state.board.push(move)
 
-        # # update tracking stuff
-        # origin = move[:2]
-        # end = move[2:]
+        # update tracking stuff
+        origin = move[:2]
+        end = move[2:]
+
+        origin_piece = state.locations[origin]
+        end_piece = state.locations[end]        # if the piece at end gets eaten, otherwise None
         
-        # piece = state.locations[origin]
-        # state.locations[origin] = None
 
-        # state.locations[end] = piece
+        state.locations[end] = origin_piece     # move the piece at origin into the new square
+        state.locations[origin] = None          # nothing in the recently emptied square
 
-    def undo_move(self, state):
+        # need to remove the piece that got eaten at end location
+        if self.color == chess.WHITE:
+            state.black_pieces.index
+
+
+        return (origin_piece, end_piece)        # return the pieces we changed so we can undo its location later, if needed
+       
+    def undo_move(self, state, move, origin_piece, end_piece):
         state.board.pop()
         state.depth -= 1
+
+        # update tracking stuff
+        origin = move[:2]
+        end = move[2:]
+
+        # put pieces (or None) back where they were
+        state.locations[origin] = origin_piece 
+        state.locations[end] = end_piece    
 
     def find_pieces(self, state):
         white = []
