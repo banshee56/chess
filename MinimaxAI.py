ba298f1162f68591d69ec90ce27f129fc5dd41e1
Bansharee Ireen <bansharee@Bansharees-MacBook-Pro.local>
2022-10-22 20:02:32 -0400

    set up minimax with dummy cutoff function

--- MinimaxAI.py
+++ MinimaxAI.py
@@ -1,29 +1,99 @@
 import chess
+from random import shuffle
+
+class State():
+    def __init__(self, board):
+        self.board = board
+        self.depth = 0
+        
 
 class MinimaxAI():
     def __init__(self, depth):
         self.depth_limit =  depth
-    
-    def get_successors(self, state):
-        # get_successors functions
-        pass
+
+    def choose_move(self, board):
+        state = State(board)
+        move = self.minimax_decision(state)
+        print("Minimax AI recommending move " + str(move))
+        return move
+
+    # def evaluation_fn(self, state):
 
     # checks if the state is a terminal state or not
     def terminal_test(self, state):
-        children = self.get_successors(state)
-        
-        # no children -> terminal state
-        if not children:
+        outcome = state.board.outcome()
+
+        # no win or draw
+        if outcome is None:
+            return False
+        # checkmate or draws
+        elif outcome.termination.value >= 1:
             return True
         
-        # not terminal state
         return False
 
-    def choose_move(self, board):
-        pass
+    # cutoff when we reach terminal state with a win or draw
+    # or when we hit max depth
+    def cutoff_test(self, state):
+        if self.terminal_test(state) or state.depth > self.depth_limit:
+            return True
+        return False
+
+    def minimax_decision(self, state):
+        board = state.board
+        arg_max = -float('inf')
+        chosen_move = None
+
+        moves = list(board.legal_moves)
+        shuffle(moves)
+        for move in moves:
+            state.depth += 1
+
+            board.push(move)
+            utility = self.min_value(state)
+
+            state.depth -= 1
+
+            if utility >  arg_max:
+                arg_max = utility
+                chosen_move = move
+            
+            board.pop()
+        
+        return chosen_move
 
     def max_value(self, state):
-        pass
+        if self.cutoff_test(state):
+            return self.evaluation_fn(state)
+        
+        board = state.board
+        v = -float('inf')
+
+        for move in list(board.legal_moves):
+            state.depth += 1
+
+            board.push(move)
+            v = max(v, self.min_value(state))
+            board.pop()
+
+            state.depth -= 1
+
+        return v
 
     def min_value(self, state):
-        pass
+        if self.cutoff_test(state):
+            return self.evaluation_fn(state)
+        
+        board = state.board
+        v = float('inf')
+
+        for move in list(board.legal_moves):
+            state.depth += 1
+
+            board.push(move)
+            v = min(v, self.max_value(state))
+            board.pop()
+
+            state.depth -= 1
+
+        return v
--- test_chess.py
+++ test_chess.py
@@ -12,7 +12,8 @@ from ChessGame import ChessGame
 import sys
 
 
-player1 = HumanPlayer()
+player1 = MinimaxAI(3)
+# player1 = RandomAI()
 player2 = RandomAI()
 
 game = ChessGame(player1, player2)
@@ -21,5 +22,9 @@ while not game.is_game_over():
     print(game)
     game.make_move()
 
+# board = chess.Board("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
+# board = chess.Board("rnb1k1nr/pppp1ppp/8/2b1p3/4P3/2N2N2/pppp1qpp/R1BQKB1R w")
+# print(board)
+# print(board.outcome())
 
 #print(hash(str(game.board)))
